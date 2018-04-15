e342bdd6ce7268c2960b69046b60a95f4626e1e3
Felice Forby <feliceforby@gmail.com>
2018-04-15 22:33:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,6 +4,8 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
+    # Send the value to the given block and 
+    # store the return value into the array
     block_return_values << yield(collection[i])
     i += 1
   end
